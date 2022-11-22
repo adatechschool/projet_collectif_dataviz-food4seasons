c0130efe95753149058ce3bb49948e1a7e432aea
NewMBW <115166381+NewMBW@users.noreply.github.com>
2022-11-22 15:20:10 +0100

    extraction data

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-var a = 2;
+fetch("https://data.iledefrance.fr/api/records/1.0/search/?dataset=cueillettes-a-la-ferme-en-ile-de-france&q=")
+    .then(reponse => (reponse.json()))
+    .then(reponse2 => {
+        console.log(reponse2.records[0])
+        for (i = 0 ; i < reponse2.records.length ; i++) {
+        console.log(reponse2.records[i].fields.nom)
+            document.getElementById("nom").innerHTML += reponse2.records[i].fields.nom + "<BR>" + "<BR>" + reponse2.records[i].fields.email + "<BR>" + "<BR>" + reponse2.records[i].fields.tel + "<BR>" + "<BR>"
+        }
+    })
