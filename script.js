ba396e3864a080ed3880033c4bb8a8959c4393a2
cecess <115542299+cecess@users.noreply.github.com>
2022-11-27 08:43:46 +0100

    test code API

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ fetch("https://data.iledefrance.fr/api/records/1.0/search/?dataset=cueillettes-a
         console.log(reponse2.records[0])
         for (i = 0 ; i < reponse2.records.length ; i++) {
         console.log(reponse2.records[i].fields.nom)
-            document.getElementById("nom").innerHTML += reponse2.records[i].fields.nom + "<BR>" + "<BR>" + reponse2.records[i].fields.email + "<BR>" + "<BR>" + reponse2.records[i].fields.tel + "<BR>" + "<BR>"
+            
+            let champs = reponse2.records[i].fields
+            let nom =champs.nom;
+            let adresse =champs.adresse;
+            let code_postale =champs.cp;
+            let ville =champs.commune;
+            let email =champs.email;
+            let tel =champs.tel;
+            let site_web =champs.web;
+        
+
+
+            document.getElementById("entreprise-cueillettes").innerHTML +=
+            "div class='entreprise'>" +
+            "h2 class =nom_entreprise>"+nom +"</h2>" +
+            "<div class= 'adresse-complete'>"+adresse +"," +code_postale +"," +ville + "</div>" +
+            "<div class= 'email'>" +email + "</div>" +
+            "<div class= 'web'>Site web :<a href=" +site_web+" target='_blank'>" + site_web + "</a></div>" +
         }
     })
+    /*document.getElementById("entreprise-cueillettes").innerHTML += "<h2>"+reponse2.records[i].fields.nom + "<BR>" + "<BR>" + reponse2.records[i].fields.email + "<BR>" + "<BR>" + reponse2.records[i].fields.tel + "<BR>" + "<BR>" 
+        */
